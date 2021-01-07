47a3ccf69fd49d72b1382134e6057caf222b4042
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-07 16:04:12 +0200

    fix migration errors

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_01_06_133648) do
+ActiveRecord::Schema.define(version: 2021_01_06_153356) do
+
+  create_table "comments", force: :cascade do |t|
+    t.text "Body"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.text "title"
