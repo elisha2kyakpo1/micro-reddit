10a6d3d1d99b60316e0ec14e131f354fc164e453
elisha2kyakpo1 <kyakopoe@gmail.com>
2021-01-08 10:06:16 +0200

    fix migration error

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ApplicationRecord
   belongs_to :user, foreign_key: true
   has_many :comments, dependent: :destroy
 
-  validates :name, presence: true, length: { minimum: 10 }
-  validates :email, presence: true, length: { minimum: 10 }
+  validates :title, presence: true, length: { minimum: 10 }
+  validates :description, presence: true, length: { minimum: 20 }
 end
